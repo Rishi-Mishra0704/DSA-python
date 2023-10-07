7a89d7b2a83d0faaba222e34c0dd58d97cd6b5c7
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-10-08 01:39:40 +0530

    basic stack problem

---
+++ Stack/Baseball.py
@@ -0,0 +1,24 @@
+# Time: O(N)
+# Space: O(N)
+
+from typing import List
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+        
+        for op in operations:
+            if op == "C":
+                stack.pop()
+            elif op == "D":
+                stack.append(2 * stack[-1])
+            elif op == "+":
+                stack.append(stack[-1] + stack[-2])
+            else:
+                stack.append(int(op))
+        
+        return sum(stack)
+    
+
+sol = Solution()
+operations = ["5","2","C","D","+","4","9","+","+"]
+print(sol.calPoints(operations))
